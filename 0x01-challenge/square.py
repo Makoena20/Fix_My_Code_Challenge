4e70276d6cb07f44e99746d501c10be760c7d888
Makoena20 <koenaletseku@gmail.com>
2024-05-27 07:16:53 +0000

    square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,29 +1,13 @@
-#!/usr/bin/python3
-""" Module for Square class """
+class Square:
+    """Class Square defines a square"""
 
-class Square():
-    """ Square class """
-    def __init__(self, *args, **kwargs):
-        """ Instantiation of class """
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width):
+        """Instantiation with width"""
+        if not isinstance(width, (int, float)) or width < 0:
+            raise ValueError("Width must be a non-negative number")
+        self.__width = width
 
     def area(self):
-        """ Area of the square """
-        return self.width * self.width
-
-    def perimeter(self):
-        """ Perimeter of the square """
-        return self.width * 4
-
-    def __str__(self):
-        """ Printable representation """
-        return "{}/{}".format(self.width, self.width)
-
-if __name__ == "__main__":
-    """ Create a square object """
-    s = Square(width=12)
-    print(s)
-    print(s.area())
-    print(s.perimeter())
+        """Returns the area of the square"""
+        return self.__width * self.__width
 
